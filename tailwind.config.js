b8e2a41bc8e6be76e2c2218e12e322307c70e838
Tom Hine <tomhine792@gmail.com>
2022-06-24 09:17:04 +0100

    added style-guide and colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      primary: {
+        cyan: "hsl(172, 67%, 45%)",
+        darkcyan: "hsl(183, 100%, 15%)",
+        lightcyan: "hsl(173, 61%, 77%)",
+      },
+      secondary: {
+        bg: "hsl(185, 41%, 84%)",
+        inputbg: "hsl(202, 50%, 97%)",
+        heading: "hsl(183, 21%, 34%)",
+        text: "hsl(186, 9% 45%)",
+      },
+    },
   },
   plugins: [],
 };
