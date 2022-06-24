ffd5cf28845999bfe4b795a17c1d6701adccf6f8
Tom Hine <tomhine792@gmail.com>
2022-06-24 14:31:47 +0100

    started building out tip details section

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div class="mx-auto w-full max-w-5xl space-y-9 lg:space-y-22">
+      <header class="flex items-center justify-center">
+        <h1 class="text-lg uppercase tracking-[0.6em] text-secondary-heading">
+          spli
+          <br />
+          tter
+        </h1>
       </header>
+      <main class="grid w-full rounded-[2rem] bg-white p-7 lg:grid-cols-2">
+        <div class="p-4">
+          <div class="flex flex-col gap-2">
+            <label for="bill">Bill</label>
+            <div class="flex items-center justify-between rounded-lg bg-secondary-inputbg px-4 py-1.5">
+              <div></div>
+              <input
+                type="number"
+                min={0.0}
+                class="w-min appearance-none bg-transparent text-2xl outline-none placeholder:text-right"
+                style="-moz-appearance: textfield"
+                placeholder="0.00"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,24 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      primary: {
-        cyan: "hsl(172, 67%, 45%)",
-        darkcyan: "hsl(183, 100%, 15%)",
-        lightcyan: "hsl(173, 61%, 77%)",
+      colors: {
+        primary: {
+          cyan: "hsl(172, 67%, 45%)",
+          darkcyan: "hsl(183, 100%, 15%)",
+          lightcyan: "hsl(173, 61%, 77%)",
+        },
+        secondary: {
+          bg: "hsl(185, 41%, 84%)",
+          inputbg: "hsl(202, 50%, 97%)",
+          heading: "hsl(183, 21%, 34%)",
+          text: "hsl(186, 9% 45%)",
+        },
       },
-      secondary: {
-        bg: "hsl(185, 41%, 84%)",
-        inputbg: "hsl(202, 50%, 97%)",
-        heading: "hsl(183, 21%, 34%)",
-        text: "hsl(186, 9% 45%)",
+      fontFamily: {
+        space: ["Space Mono", "monospace"],
+      },
+      spacing: {
+        22: "88px",
       },
     },
   },
